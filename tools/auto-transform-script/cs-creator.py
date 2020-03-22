b671e4c0b5d48f8152eb8132122d8861bd5bf49d
patrick-werner <pa.f.werner@gmail.com>
2020-03-22 17:30:24 +0100

    script proptoype ready, trying travis

---
+++ tools/auto-transform-script/cs-creator.py
@@ -0,0 +1,18 @@
+import yaml, os
+
+
+def clamlconvert(claml):
+  converter = claml["converter"]
+  for key, value in claml["clamlPackages"].items():
+    print(key)
+    os.system('java -jar tools/' + converter + ' -i ' + value[
+      "clamlfile"] + ' -designations ' + value["designations"] + ' -o generated-resources/' + value[
+                "outputFolder"] + '/' + value["outputFileName"] + ' -id ' + key + ' -url ' + value[
+                "url"] + ' -valueset ' + value["valueset"])
+
+
+with open('config.yaml') as config:
+  data = yaml.safe_load(config)
+
+  # claml handling
+  clamlconvert(data["packages"]["claml"])
